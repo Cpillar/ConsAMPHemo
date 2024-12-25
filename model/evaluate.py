f7790a1f1ee6dbf2031c3c83fa7b9aa1e978bfb0
Cpillar <73626201+Cpillar@users.noreply.github.com>
2024-12-25 15:40:19 +0800

    Update evaluate.py

--- model/evaluate.py
+++ model/evaluate.py
@@ -386,13 +386,13 @@ class FocalLoss(nn.Module):
         self.reduction = reduction
 
     def forward(self, pred, target):
-        alpha = self.alpha[target]  # 为当前batch内的样本，逐个分配类别权重，shape=(bs), 一维向量
-        log_softmax = torch.log_softmax(pred, dim=1) # 对模型裸输出做softmax再取log, shape=(bs, 3)
-        logpt = torch.gather(log_softmax, dim=1, index=target.view(-1, 1))  # 取出每个样本在类别标签位置的log_softmax值, shape=(bs, 1)
-        logpt = logpt.view(-1)  # 降维，shape=(bs)
-        ce_loss = -logpt  # 对log_softmax再取负，就是交叉熵了
-        pt = torch.exp(logpt)  #对log_softmax取exp，把log消了，就是每个样本在类别标签位置的softmax值了，shape=(bs)
-        focal_loss = alpha * (1 - pt) ** self.gamma * ce_loss  # 根据公式计算focal loss，得到每个样本的loss值，shape=(bs)
+        alpha = self.alpha[target]  
+        log_softmax = torch.log_softmax(pred, dim=1) 
+        logpt = torch.gather(log_softmax, dim=1, index=target.view(-1, 1))  
+        logpt = logpt.view(-1)  
+        ce_loss = -logpt  
+        pt = torch.exp(logpt) 
+        focal_loss = alpha * (1 - pt) ** self.gamma * ce_loss 
         if self.reduction == "mean":
             return torch.mean(focal_loss)
         if self.reduction == "sum":
